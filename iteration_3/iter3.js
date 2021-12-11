03c5bf223f1a7a5d8d54be900df1cbe6ab5d94ab
Toni M <tonims.dev@gmail.com>
2021-12-11 14:18:15 +0100

    Iteration#3

---
+++ iteration_3/iter3.js
@@ -0,0 +1,8 @@
+/*#3. 
+Usa un bucle forof para recorrer todos los destinos del array. Imprime en un ***console.log*** sus valores.*/
+
+const placesToTravel = ['Japon', 'Venecia', 'Murcia', 'Santander', 'Filipinas', 'Madagascar']
+
+for (const places of placesToTravel) {
+    console.log(places);
+}
